1880a49c5db26c8c8889dc575759e0c81928665a
kanailalmanna <kanailalmanna2003@gmail.com>
2022-05-05 00:36:15 +0530

    add to master

--- BoubleSort.java
+++ BoubleSort.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Sortings {
+class BoubleSort {
     public static void printArray(int arr[]){
         for(int i = 0 ; i<arr.length; i++){
             System.out.print(arr[i] + " ");
