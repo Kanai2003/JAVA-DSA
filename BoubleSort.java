a6f10fa41755f283ff9026432f38c10f4a7fc63b
kanailalmanna <kanailalmanna2003@gmail.com>
2022-05-07 19:24:19 +0530

    push to master

--- BoubleSort.java
+++ BoubleSort.java
@@ -20,25 +20,6 @@ class BoubleSort {
                 }
             }
         }
-
-        
-        
-
-
-        //Insertion Sort ----
-            for (int i=1; i<arr.length; i++){
-                int current = arr[i];
-                int j = i-1;
-                while(j>=0 && current < arr[j]){
-                    arr[j+1] = arr[j];
-                    j--;
-                }
-                //placement
-                arr[j+1]=current;
-            }
-
-
-        
         printArray(arr);
     }
 }
---
+++ InsertionSort.java
@@ -0,0 +1,26 @@
+public class InsertionSort {
+    public static void printArray(int arr[]){
+        for(int i = 0 ; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 1, 2, 3};
+        //Insertion Sort ----
+        for (int i=1; i<arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            //placement
+            arr[j+1]=current;
+        }
+
+
+    
+    printArray(arr);
+    }
+}
