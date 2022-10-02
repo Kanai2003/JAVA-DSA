6a1468d8e61d0904a7ab43f9be624285d4e7c7f2
Kanailal Manna <91842029+KanailalManna@users.noreply.github.com>
2022-10-02 22:07:00 +0530

    hacktoberfest

--- Sorting/MargeSort.java
+++ Sorting/MargeSort.java
@@ -5,32 +5,7 @@ public class MargeSort {
 
     
     
-    public static void conqure(int arr[],int start, int mid, int end) {
-        int mar[]=new int[end -start+1];
-        int idx1 = start;
-        int idx2 = mid+1;
-        int x = 0;
-        while(idx1 <= mid && idx2 <= end){      
-            if(arr[idx1] <= arr[idx2]){
-                mar[x++] = arr[idx1++];
-            }else{
-                mar[x++] = arr[idx2++];
-            }
-        }
-
-        while (idx1 <= mid){
-            mar[x++] = arr[idx1++];
-        }
-
-        while (idx2 <= end){
-            mar[x++] = arr[idx2++];
-        }
-
-        for(int i=0,j=start ; i < mar.length ; i++,j++){
-            arr[j]= mar[i];
-        }
-    }
-
+    
     public static void divide(int arr[] , int start , int end){
         if(start >= end){
             return;
